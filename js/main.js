d24b61b6807a59d233ee72fa1363cab634286a04
j3ongjisu <132436950+j3ongjisu@users.noreply.github.com>
2023-05-31 16:09:52 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,9 +1,46 @@
 $(function () {
 
+    $('.main_slide').on('init afterChange', function (e, s, c) {
+        const current = $('.main_slide .slick-current');
+        current.addClass('on').siblings().removeClass('on');
+
+        //슬라이드 숫자 표시(1/3)
+        $('.main_visual .slide_num span').text(c ? (c + 1) : 1);
+        $('.main_visual .slide_num strong').text(s.slideCount);
+
+        console.log(s.slideCount);
+
+        $('.main_visual .menu li').eq(0).addClass('on');
+        $('.main_visual .menu li').eq(c).addClass('on')
+            .siblings().removeClass('on');
+    });
+
+    $('.main_slide').slick({
+        autoplay: true,
+        autoplaySpeed: 5000,
+        speed: 1000,
+        pauseOnHover: false,
+        fade: true,
+    });
+
+
+    $('.main_visual .arrows .left').on('click', function () {
+        $('.main_slide').slick('slickPrev')
+    });
+    $('.main_visual .arrows .right').on('click', function () {
+        $('.main_slide').slick('slickNext')
+    });
+
+    $('.main_visual .menu li').on('click', function (e) {
+        e.preventDefault();
+        const idx = $(this).index(); // 0,1,2
+        $('.main_slide').slick('slickGoTo', idx);
+    })
+
+
+
     $('.product_slide').slick({
-        //반응형 슬라이드 만드는 법
         slidesToShow: 3,
-        //제품 슬라이드와 pic 슬라이드가 같이 돎.
         asNavFor: ".pic_slide",
         focusOnSelect: true,
         responsive: [
@@ -19,6 +56,21 @@ $(function () {
     $('.pic_slide').slick({
         vertical: true,
         asNavFor: ".product_slide",
+
     });
+
+    //to_top 스크롤 위로 올리기
+    $('.to_top').on('click', function () {
+        $('html, body').animate({ scrollTop: 0 }, 600)
+    });
+
+
+    //스크롤 아래로 한 칸 내리기
+    $('.scr').on('click', function (e) {
+        e.preventDefault();
+        const st = $(this.hash).offset().top;
+        console.log(st);
+        $('html, body').animate({ scrollTop: st }, 600)
+    })
 
 })
